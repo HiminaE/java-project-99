9dcd1bd09a25007f9dadda001c97fad02c923404
HiminaE <himinaev@yandex.ru>
2024-08-03 09:43:17 +0300

    Add build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,85 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.3.3-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.1.6"
+	application
+	checkstyle
+	id("jacoco")
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("io.freefair.lombok") version "8.6"
+	id("io.sentry.jvm.gradle") version "4.8.0"
 }
 
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+	sourceCompatibility = JavaVersion.VERSION_20
 }
 
 repositories {
 	mavenCentral()
-	maven { url = uri("https://repo.spring.io/snapshot") }
 }
+application { mainClass.set("hexlet.code.AppApplication") }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter:3.3.2")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.3.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.1")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.2")
+	runtimeOnly("com.h2database:h2:2.2.224")
+	runtimeOnly("org.postgresql:postgresql:42.7.3")
+
+	implementation("net.datafaker:datafaker:2.3.0")
+	implementation("org.instancio:instancio-junit:5.0.1")
+	testImplementation(platform("org.junit:junit-bom:5.10.3"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
+
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
+	//for security, jwt
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	//end security, jwt
+
+	//swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 }
 
-tasks.withType<Test> {
+tasks.test {
 	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn (tasks.test)
+	reports {
+		xml.required = true
+		csv.required = false
+	}
+}
+buildscript {
+	repositories {
+		mavenCentral()
+	}
+}
+
+sentry {
+	// Generates a JVM (Java, Kotlin, etc.) source bundle and uploads your source code to Sentry.
+	// This enables source context, allowing you to see your source
+	// code as part of your stack traces in Sentry.
+	includeSourceContext = true
+	org = "home-sti"
+	projectName = "java-spring-boot"
+	authToken = System.getenv("SENTRY_AUTH_TOKEN")
+}
+
+tasks.sentryBundleSourcesJava {
+	enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
 }
