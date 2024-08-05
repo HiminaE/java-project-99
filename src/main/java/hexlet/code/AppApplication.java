cf165ab371368f314ac0b842e706f5c1d7fa4ce2
HiminaE <155444135+HiminaE@users.noreply.github.com>
2024-08-05 14:16:05 +0300

    Update AppApplication.java

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -13,7 +13,7 @@ public class AppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
-	}
+    }
 
 //	@Bean
 //	public Faker getFaker() {
