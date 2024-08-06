facf7d67e55ca357a2e8451dbedd56d894f13000
HiminaE <himinaev@yandex.ru>
2024-08-06 16:04:45 +0300

    corrected build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,16 @@ plugins {
 	application
 	checkstyle
 	id("jacoco")
-	id("org.springframework.boot") version "3.2.2"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("org.springframework.boot") version "3.3.1"
+	id("io.spring.dependency-management") version "1.1.5"
 	id("io.freefair.lombok") version "8.6"
-	id("io.sentry.jvm.gradle") version "4.8.0"
+	id("com.github.ben-manes.versions") version "0.50.0"
+	id("io.sentry.jvm.gradle") version "4.10.0"
 }
 application {mainClass.set("hexlet.code.AppApplication")}
 
 group = "hexlet.code"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
 	mavenCentral()
@@ -30,17 +31,17 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
 	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
 
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.1")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.2.7")
 
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 
 	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-	testImplementation("org.springframework.security:spring-security-test")
-	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server:3.3.0")
+	testImplementation("org.springframework.security:spring-security-test:6.3.0")
+	implementation("org.springframework.boot:spring-boot-devtools:3.3.0")
 
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 }
