2a71677cbac04d432c03c2588980f993d9fa539b
HiminaE <himinaev@yandex.ru>
2024-08-06 14:57:14 +0300

    add AuthenticationController

---
+++ src/main/java/hexlet/code/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package hexlet.code.controller;
+
+
+import hexlet.code.dto.AuthRequestDTO;
+import hexlet.code.util.JWTUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class AuthenticationController {
+
+    private final JWTUtils jwtUtils;
+
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public String create(@RequestBody AuthRequestDTO authRequest) {
+        var authentication = new UsernamePasswordAuthenticationToken(
+                authRequest.getUsername(), authRequest.getPassword());
+
+        authenticationManager.authenticate(authentication);
+
+        return jwtUtils.generateToken(authRequest.getUsername());
+    }
+}
