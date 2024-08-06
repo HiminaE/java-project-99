9119a7e301d52e16a235fa610776fa914b0683fe
HiminaE <himinaev@yandex.ru>
2024-08-06 15:17:03 +0300

    add Access

---
+++ src/main/java/hexlet/code/exception/AccessViolationException.java
@@ -0,0 +1,8 @@
+package hexlet.code.exception;
+
+
+public class AccessViolationException extends RuntimeException {
+    public AccessViolationException(String message) {
+        super(message);
+    }
+}
