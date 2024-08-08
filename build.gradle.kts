fd88ecd05aae930544be31e06a59928c14ec441d
HiminaE <155444135+HiminaE@users.noreply.github.com>
2024-08-08 14:04:48 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,32 +18,34 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter:3.3.2")
-	implementation("org.springframework.boot:spring-boot-starter-web:3.3.2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.1")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.2")
-	runtimeOnly("com.h2database:h2:2.2.224")
-	runtimeOnly("org.postgresql:postgresql:42.7.3")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-devtools")
+    implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
-	implementation("net.datafaker:datafaker:2.3.0")
-	implementation("org.instancio:instancio-junit:5.0.1")
-	testImplementation(platform("org.junit:junit-bom:5.10.3"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
-	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
 
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.1")
-	implementation("org.springframework.boot:spring-boot-starter-security:3.2.7")
+    implementation("org.instancio:instancio-junit:3.6.0")
+    implementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    implementation("net.datafaker:datafaker:2.0.2")
 
-	implementation("org.mapstruct:mapstruct:1.5.5.Final")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
 
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server:3.3.0")
-	testImplementation("org.springframework.security:spring-security-test:6.3.0")
-	implementation("org.springframework.boot:spring-boot-devtools:3.3.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 }
 
 sentry {
