a5bfbc4a640e0fc391e21d4cf7d5778f9c89a0f9
HiminaE <155444135+HiminaE@users.noreply.github.com>
2024-08-13 18:13:39 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("com.github.ben-manes.versions") version "0.48.0"
     id("application")
@@ -9,66 +12,96 @@ plugins {
     id("io.freefair.lombok") version "8.4"
 }
 
-}
-application {mainClass.set("hexlet.code.AppApplication")}
-
 group = "hexlet.code"
 version = "0.0.1-SNAPSHOT"
 
-repositories {
-	mavenCentral()
+application {
+    mainClass.set("hexlet.code.AppApplication")
 }
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("org.springframework.boot:spring-boot-devtools")
-    implementation("org.mapstruct:mapstruct:1.6.0.Beta1")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta1")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
 
-    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+checkstyle {
+    configFile = file("config/checkstyle/checkstyle.xml")
+    toolVersion = "10.13.0"
+}
 
-    implementation("org.instancio:instancio-junit:3.6.0")
-    implementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
-    implementation("net.datafaker:datafaker:2.0.2")
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
 
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("org.postgresql:postgresql")
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 sentry {
+    includeSourceContext = true
 
-	includeSourceContext = true
-
-	org = "personal-use-jw"
-	projectName = "java-spring-boot"
-	authToken = System.getenv("SENTRY_AUTH_TOKEN")
-}
-
-tasks.test {
-	useJUnitPlatform()
-}
-
-tasks.jacocoTestReport {
-	reports {
-		xml.required.set(true)
-	}
+    org = "hexlet-p5"
+    projectName = "java-spring-boot"
+    authToken = System.getenv("SENTRY_AUTH_TOKEN")
 }
 
 tasks.sentryBundleSourcesJava {
-	enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
+    enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
+}
+
+dependencies {
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    implementation("org.instancio:instancio-junit:3.3.1")
+    implementation("net.datafaker:datafaker:2.0.2")
+    testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.2")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.2.0")
+    testImplementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.2.0")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("net.sf.ehcache:ehcache:2.10.6")
+    implementation("org.ehcache:ehcache:3.10.8")
+    implementation("org.ehcache:ehcache-transactions:3.10.0")
+    implementation("javax.cache:cache-api:1.1.1")
+    implementation("org.springframework:spring-context-support:5.3.25")
+    implementation("net.sf.ehcache:ehcache-core:2.6.10")
+
+}
+
+tasks.withType<Test>() {
+    finalizedBy(tasks.jacocoTestReport)
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
 }
